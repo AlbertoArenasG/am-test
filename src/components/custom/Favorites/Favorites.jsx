6a4efa126382ed1913ea83b636c8fcef19b49ed8
Alberto Arenas <albarenasg@gmail.com>
2021-12-09 20:57:37 -0600

    feature/home-page-functionality: favorites dropdown

--- src/components/custom/Favorites/Favorites.jsx
+++ src/components/custom/Favorites/Favorites.jsx
@@ -1,9 +1,64 @@
-import React, { memo } from 'react';
-import './Favorites.scss'
+import React, { memo, useState } from "react";
+import "./Favorites.scss";
+import { useSelector } from "react-redux";
+import { useHogwartsActions } from "../../../redux/actions/useHogwartsActions";
+import { API } from "../../../helpers/http.helper";
+import MarkWhite from "../../../assets/icons/mark-white.svg";
+import Add from "../../../assets/icons/add.svg";
+import Trash from "../../../assets/icons/trash.svg";
+
 const Favorites = memo(() => {
+  const api = API();
+  const { setFavoritesData } = useHogwartsActions();
+  const favorites = useSelector((state) => state.hogwarts.favorites);
+  const [visibility, setVisibility] = useState(true);
+
+  const handleDropdownVisibility = () => {
+    setVisibility(!visibility);
+  };
+
+  const deleteCharFromFavorite = (char) => {
+    let newArray = favorites.filter((item) => item.id !== char.id);
+    api.remove(`favorites/${char.id}`).then((res) => {});
+    setFavoritesData(newArray);
+  };
+
+  const renderFavorites = favorites.map((res, key) => {
+    let Style = {};
+    Style = { ...Style, backgroundImage: `url(${res.image})` };
+    let Style2 = {};
+    if(key===favorites.length-1)Style2 = { ...Style2, border:'0' };
+    return (
+      <div key={key} className="favorite-item" style={Style2}>
+        <div className="name">
+          <div style={Style} className="avatar" />
+          <span>{res.name}</span>
+        </div>
+
+        <div className="image" onClick={()=>deleteCharFromFavorite(res)}>
+          <img src={Trash} alt="trash" />
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div className='favorites'>
-      Favorites
+    <div className="favorite-dropdown-container">
+      <div className="favorite-dropdown">
+        <div className="tab" onClick={handleDropdownVisibility}>
+          FAVORITOS
+          <img src={MarkWhite} alt="mark" />
+        </div>
+
+        <div className="tab">
+          AGREGAR
+          <img src={Add} alt="mark" />
+        </div>
+      </div>
+
+      <div className={`dropdown-content ${visibility ? "visible" : ""}`}>
+        {renderFavorites}
+      </div>
     </div>
   );
 });
